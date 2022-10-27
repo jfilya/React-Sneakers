4add54619f204c18abce7a27d20806bfd4cff4f5
Julia Filippova <julia.94@bk.ru>
2022-10-27 16:29:24 +0300

    feat: add search sneakers

--- react-sneakers/src/App.js
+++ react-sneakers/src/App.js
@@ -7,6 +7,7 @@ import React from "react";
 function App() {
   const [items, setItems] = React.useState([]);
   const [cartItems, setCartItems] = React.useState([]);
+  const [searchValue, setSearchValue] = React.useState("");
   const [cartOpened, setCartOpened] = React.useState(false);
 
   React.useEffect(() => {
@@ -16,34 +17,56 @@ function App() {
   }, []);
 
   const addToCart = (el) => {
-    
     setCartItems((prev) => [...prev, el]);
-  }
+  };
+
+  const onChangeSearchInput = (event) => {
+    setSearchValue(event.target.value);
+  };
 
   return (
     <div className="wrapper">
-      {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)} />}
+      {cartOpened && (
+        <Drawer items={cartItems} onClose={() => setCartOpened(false)} />
+      )}
       <Header onClickCart={() => setCartOpened(true)} />
       <section className="content">
         <div className="content__top">
-          <h1>Все кроссовки</h1>
+          <h1>{searchValue ? `Поиск: ${searchValue}` : `Все кроссовки`}</h1>
           <div className="content__search-block">
             <img src="img/search.svg" alt="search" />
-            <input type="text" placeholder="Поиск..." />
+            {searchValue && (
+              <img
+                className="content__search-clear cursor"
+                src="img/btn-remove.svg"
+                alt="clear"
+                onClick={() => setSearchValue("")}
+              />
+            )}
+            <input
+              onChange={onChangeSearchInput}
+              type="text"
+              placeholder="Поиск..."
+              value={`${searchValue}`}
+            />
           </div>
         </div>
 
         <div className="content__cards">
-          {items.map((item) => (
-            <Card
-              key={item.name + item.price}
-              name={item.name}
-              price={item.price}
-              imgUrl={item.img}
-              addToFavorite={() => console.log(item)}
-              addToBasket={(obj) => addToCart(obj)}
-            />
-          ))}
+          {items
+            .filter((item) =>
+              item.name.toLowerCase().includes(searchValue.toLowerCase())
+            )
+            .map((item) => (
+              <Card
+                key={item.name + item.price}
+                name={item.name}
+                price={item.price}
+                imgUrl={item.img}
+                addToFavorite={() => console.log(item)}
+                addToBasket={(obj) => addToCart(obj)}
+              />
+            ))}
         </div>
       </section>
     </div>
